fb93c8fc8bd5e35ed8d0f0776860488367451660
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-05-19 20:54:52 +0300

    update readme

--- src/RL/Startup.cs
+++ src/RL/Startup.cs
@@ -6,12 +6,8 @@ using DAL;
 using DAL.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace RL
@@ -39,7 +35,6 @@ namespace RL
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RL v1"));
 
