af61b946f13dfc5102004b9f8f958c656405ac64
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-05-19 20:44:56 +0300

    change connection string

--- src/DAL/ApplicationContext.cs
+++ src/DAL/ApplicationContext.cs
@@ -7,6 +7,9 @@ namespace DAL
 {
     public class ApplicationContext : DbContext
     {
+        private const string HerokuConnectionString =
+            "Host=ec2-54-216-185-51.eu-west-1.compute.amazonaws.com;Database=dfr68d3vuhel57;Username=plcarodozmppsj;Password=a19cfcb97a057e9b1228be58c7cee68c07693692ea608320de3b53edde63685d;sslmode=Require;TrustServerCertificate=true";
+        
         private const string LocalConnectionString =
             "Host=localhost;Database=test_task_db;Username=postgres;Password=qweasd123";
         
@@ -21,7 +24,7 @@ namespace DAL
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql(LocalConnectionString);
+                optionsBuilder.UseNpgsql(HerokuConnectionString);
             }
         }
 
